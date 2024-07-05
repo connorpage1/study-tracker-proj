0ea9f7d5d3582353f546ed98190dd56fa2fc122c
Connor Page <george@connorpage.com>
2024-07-05 16:18:37 -0500

    Adds comments, changes favicon

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
+/* This project was created by Connor Page for the Phase 2 final project 
+at Flatiron. For more information, see the README. */
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './routes/index.jsx'
