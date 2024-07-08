de6b2ee2a4ff7605068116d5586ea8b0644e73cc
Connor Page <george@connorpage.com>
2024-07-08 14:35:46 -0500

    Pre-assesment code

--- src/components/main/NavBar.jsx
+++ src/components/main/NavBar.jsx
@@ -3,9 +3,9 @@ import { NavLink } from "react-router-dom"
 const NavBar = () => {
     return (
         <div id="nav-bar">
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/sessions">Sessions</NavLink>
-            <NavLink to="/sessions/new">Log a new session</NavLink>
+            <NavLink to="/" className={({ isActive }) => isActive ? "active": ''}>Home</NavLink>
+            <NavLink to="/sessions" className={({ isActive }) => isActive ? "active": ''}>Sessions</NavLink>
+            <NavLink to="/sessions/new" className={({ isActive }) => isActive ? "active": ''}>Log a new session</NavLink>
         </div>
 
     )
---
+++ src/components/table/ExpandedView.jsx
@@ -0,0 +1,18 @@
+const ExpandedView = () => {
+    return (
+        <div>
+            <h2>Session Details:</h2>
+            <h4>Date: </h4>
+            <h4>Start Time: </h4>
+            <h4>Duration: </h4>
+            <h4>Location: </h4>
+            <h4>Subject: </h4>
+            <h4>Description: </h4>
+            <h4>Focus Level: </h4>
+            <h4>Phone Bricked?: </h4>
+        </div>
+        
+    )
+}
+
+export default ExpandedView;
